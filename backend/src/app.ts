e388b14fa49d03539ab6b49f107302f627c3837e
Connor Richard Davis <st10068305@vcconnect.edu.za>
2024-10-07 15:50:45 +0200

    Cleanup API initial commit

--- backend/src/app.ts
+++ backend/src/app.ts
@@ -1,5 +1,5 @@
 import { OpenAPIHono } from "@hono/zod-openapi";
-import { notFound, onError } from "stoker/middlewares";
+import { notFound, onError, serveEmojiFavicon } from "stoker/middlewares";
 
 import { pinoLogger } from "./middleware/pino-logger";
 import indexRouter from "./routes";
@@ -7,11 +7,21 @@ import { Config } from "./types/config";
 
 const app = new OpenAPIHono<Config>();
 
+/**
+ * Setup our not found and error middleware to return JSON responses.
+ */
 app.notFound(notFound);
 app.onError(onError);
 
+/**
+ * Setup our usages.
+ */
+app.use(serveEmojiFavicon("📦"));
 app.use(pinoLogger());
 
+/**
+ * Initialize our router.
+ */
 app.route("/", indexRouter);
 
 export default app;
